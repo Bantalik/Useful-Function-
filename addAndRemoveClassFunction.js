ae7999bedd69bbf49a7ecd2320dd8041f6d0f964
Bantalik <divin.josef@seznam.cz>
2018-09-05 16:21:31 +0200

    Function to add and remove some classes from HTML

---
+++ addAndRemoveClassFunction.js
@@ -0,0 +1,18 @@
+function addAndRemoveClass(divElement, addClas, removeClas){
+    //Add and remove some specific class on html element
+    /*
+    For select parametr "divElemnt" and "toggle" use css selector anotation for instant for ID chose (#)
+    divElement - specific html element where we want to change classes (string)
+    addClas - add clases (string)
+    removeClas - remove classes (string)
+    */
+    var divElemente = document.querySelector(divElement);
+    
+    function addRemove(){
+        
+        divElemente.classList.add(addClas);
+        divElemente.classList.remove(removeClas);
+        
+    }
+    divElemente.addEventListener('click', addRemove, false);
+}
